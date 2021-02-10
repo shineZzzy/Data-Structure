0a32e043c349e2a270794a0510f23ded0112d4b4
Ash-1024 <1468735293@qq.com>
2021-02-10 23:53:59 +0800

    买卖股票

---
+++ EveryDay/Day36/solution.java
@@ -0,0 +1,18 @@
+package Test.Day36;
+//leetcode 官方题解
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp0 = 0, dp1 = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            int newDp0 = Math.max(dp0, dp1 + prices[i]);
+            int newDp1 = Math.max(dp1, dp0 - prices[i]);
+            dp0 = newDp0;
+            dp1 = newDp1;
+        }
+        return dp0;
+    }
+}
+
+
+//链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/solution/mai-mai-gu-piao-de-zui-jia-shi-ji-ii-by-leetcode-s/
