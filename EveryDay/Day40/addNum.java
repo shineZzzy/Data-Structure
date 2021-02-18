85e0cc365f575655dd814ba40daa389501ae21b7
Ash-1024 <1468735293@qq.com>
2021-02-19 00:10:04 +0800

    两数相加 链表

---
+++ EveryDay/Day40/addNum.java
@@ -0,0 +1,37 @@
+package Test.Day40;
+
+public class addNum {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //创建新链表的伪头节点
+        ListNode head=new ListNode(0);
+        ListNode curr=head;
+        //记录进位：满十进一
+        int carry=0;
+        while (l1!=null || l2!=null){
+            //当l1 l2各不为空时的值
+            int n1=l1!=null?l1.val:0;
+            int n2=l2!=null?l2.val:0;
+            //求和
+            int sum=carry+n1+n2;
+            //更新carry
+            carry=sum/10;
+            //确定下一个结点的位置，及其值
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
+            //当l1 或 l2不为空时，继续往后走
+            //若有一个为空，则将另一个的值按序补在新链表后面
+            if (l1!=null){
+                l1=l1.next;
+            }
+            if (l2!=null){
+                l2=l2.next;
+            }
+        }
+        //如果链表遍历结束后，有 carry>0，还需要在答案链表的后面附加一个节点，节点的值为carry。
+        if (carry>0){
+            curr.next=new ListNode(carry);
+        }
+        //返回新链表的头节点
+        return head.next;
+    }
+}
---
+++ EveryDay/Day40/addNumber.java
@@ -0,0 +1,80 @@
+package Test.Day40;
+
+/**
+ *给你两个 非空 的链表，表示两个非负的整数。
+ * 它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字
+ *
+ */
+
+
+class ListNode{
+   int val;
+   ListNode next;
+   ListNode() { }
+   ListNode(int val){
+       this.val=val;
+   }
+    ListNode(int val, ListNode next) {
+       this.val = val; this.next = next;
+   }
+}
+public class addNumber {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head=new ListNode(0);
+        ListNode cur=head;
+        ListNode p=l1;
+        ListNode q=l2;
+
+        int carry=0;
+        while (p!=null || q!=null){
+            int n1=(p!=null) ? p.val:0;
+            int n2=(q!=null) ? q.val:0;
+            int sum=carry+n1+n2;
+            carry=sum/10;
+            cur.next=new ListNode(sum%10);
+            cur=cur.next;
+            if(p!=null){
+                p=p.next;
+            }
+            if(q!=null){
+                q=q.next;
+            }
+        }
+        if(carry>0){
+            cur.next=new ListNode(carry);
+        }
+        return head.next;
+
+    }
+
+    //public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    //        ListNode head = null, tail = null;
+    //        int carry = 0;
+    //        while (l1 != null || l2 != null) {
+    //            int n1 = l1 != null ? l1.val : 0;
+    //            int n2 = l2 != null ? l2.val : 0;
+    //            int sum = n1 + n2 + carry;
+    //            if (head == null) {
+    //                head = tail = new ListNode(sum % 10);
+    //            } else {
+    //                tail.next = new ListNode(sum % 10);
+    //                tail = tail.next;
+    //            }
+    //            carry = sum / 10;
+    //            if (l1 != null) {
+    //                l1 = l1.next;
+    //            }
+    //            if (l2 != null) {
+    //                l2 = l2.next;
+    //            }
+    //        }
+    //        if (carry > 0) {
+    //            tail.next = new ListNode(carry);
+    //        }
+    //        return head;
+    //    }
+    //
+
+}
+
+
