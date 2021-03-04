a23e08a4a6183e332665caf1594d123685671839
Ash-1024 <1468735293@qq.com>
2021-03-04 22:48:53 +0800

    自定义HTTP服务器

---
+++ project/Mine/HttpServer.java
@@ -0,0 +1,33 @@
+package Mine;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+//使用多线程完成！
+public class HttpServer {
+    public static void main(String[] args) throws IOException {
+
+        //1.创建监听
+        ServerSocket serverSocket=new ServerSocket(8080);
+        while (true) {
+            //2.建立连接
+            Socket socket=serverSocket.accept();
+            Runnable runnable=new RequestResponseTask(socket);
+            new Thread(runnable).start();
+        }
+    }
+    //线程池
+    public static void main2(String[] args) throws IOException {
+        ExecutorService executorService= Executors.newFixedThreadPool(10);
+        //1.创建监听
+        ServerSocket serverSocket=new ServerSocket(8080);
+        while (true){
+            Socket socket=serverSocket.accept();
+            Runnable runnable=new RequestResponseTask(socket);
+            executorService.execute(runnable);
+        }
+    }
+}
