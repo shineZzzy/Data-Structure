c179a401cfe5339e31e784a8a70a613cff62a1d5
shineZzzy <73776492+shineZzzy@users.noreply.github.com>
2021-07-19 00:00:06 +0800

    变位词组

---
+++ proSet/Sort/T10_2.java
@@ -0,0 +1,20 @@
+package LeetCode1.Sort;
+
+import java.util.*;
+
+public class T10_2 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+
+}
