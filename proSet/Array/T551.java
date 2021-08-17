045d81dfad5bdb0e43a414f4e23e68428d96208c
shineZzzy <73776492+shineZzzy@users.noreply.github.com>
2021-08-17 23:15:49 +0800

    学生出勤记录 I

---
+++ proSet/Array/T551.java
@@ -0,0 +1,26 @@
+package LeetCode.EveryDAY.Array;
+
+public class T551 {
+    public static boolean checkRecord(String s) {
+        int absents = 0, lates = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == 'A') {
+                absents++;
+                if (absents >= 2) {
+                    return false;
+                }
+            }
+            if (c == 'L') {
+                lates++;
+                if (lates >= 3) {
+                    return false;
+                }
+            } else {
+                lates = 0;
+            }
+        }
+        return true;
+    }
+}
