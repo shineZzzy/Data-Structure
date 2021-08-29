1dd9fd2bfb8e493396df54bbf4bc7050ed3e4e92
shineZzzy <73776492+shineZzzy@users.noreply.github.com>
2021-08-29 23:57:21 +0800

    不用加减乘除做加法

---
+++ proSet/WEEK/Week1/T65/T65.java
@@ -0,0 +1,12 @@
+package LeetCode.剑指offer.Week1.T65;
+
+public class T65 {
+    public static int add(int a, int b) {
+        while(b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+}
